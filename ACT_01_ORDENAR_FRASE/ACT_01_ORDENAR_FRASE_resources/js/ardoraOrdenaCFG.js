541f6c3cffcb03e3d66df89ba49f4769d8035aab
andresfr77 <andresfr77@gmail.com>
2018-11-12 23:38:22 -0500

    primer commit

---
+++ ACT_01_ORDENAR_FRASE/ACT_01_ORDENAR_FRASE_resources/js/ardoraOrdenaCFG.js
@@ -0,0 +1,31 @@
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=1; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Muy bien hecho!!!"; messageTime=""; messageError="Lo lamento, intenta de nuevo!"; messageErrorG="Lo lamento, intenta de nuevo!"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="QUNUXzAxX09SREVOQVJfRlJBU0U"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["RW4gbGEgdmlkYSBkaWFyaWEgbm9zIGVuY29udHJhbW9zIGNvbiBtdWx0aXR1ZCBkZSBvYmpldG9zIGNyZWFkb3MgcG9yIGVsIGhvbWJyZSB5IG90cm9zIGRlIHRpcG8gbmF0dXJhbCBxdWUgc29uIG1lZGlkb3MgcGFyYSBzYXRpc2ZhY2VyIGxhcyBuZWNlc2lkYWRlcyBodW1hbmFzLg","","","",""];imaW=["","","","",""];queW=["Pista: Las siguientes palabras están en orden... Vida, Diaria, Encontramos, Multitud, Objetos, Creados, Hombre, Tipo, Natural, Medidos, Satisfacer, Necesidades, Humanas.","","","",""]; c=[160,0,0,0,0];
+var auW=["","","","",""];
+var actMaxWidth="600"; actMaxHeight="400";indexG=0;profG=0;dirMedia="ACT_01_ORDENAR_FRASE_resources/media/"; textBNext="";
+var wordsG=[];imageW=[];questionW=[];audioW=[];cG=[];
+colorText="#2B2B2B";colorButton="#5C863E";colorBack="#FFFFFF";colorSele="#A1BD8D";goURLNext=true;goURLRepeat=true;
+fMenssage="Verdana, Geneva, sans-serif";fActi="Verdana, Geneva, sans-serif";fEnun="Verdana, Geneva, sans-serif";
